b65fcee482fbaa6daff197373eda1db3b11ffade
RSAdrian9 <adrian4rs@gmail.com>
2025-05-31 14:21:16 +0200

    Archivos configuración

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,36 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
+import { createApp, h } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+// FontAwesome
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
 
+// Vuetify theme setup (opcional)
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'fa',
+        sets: {
+      fa: {
+        component: (props: any) => h('i', { class: `fa ${props.icon}` }),
+      },
+    },
+  },
+})
+
+const app = createApp(App)
+app.use(vuetify)
 app.use(createPinia())
 app.use(router)
-
 app.mount('#app')
